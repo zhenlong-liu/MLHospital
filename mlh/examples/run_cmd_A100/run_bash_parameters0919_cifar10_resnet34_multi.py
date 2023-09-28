2e6e53805d764bd1162cb266df5ba93c4e847b4b
zhenlong-liu <reallong.liu@gmail.com>
2023-09-28 21:15:22 +0800

    092328 end

--- mlh/examples/run_cmd_A100/run_bash_parameters0919_cifar10_resnet34_multi.py
+++ mlh/examples/run_cmd_A100/run_bash_parameters0919_cifar10_resnet34_multi.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
     save_merged_dicts_to_yaml(params, loss_function, "./A100_record")
     
     
-    #"""
+    """
     with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor1, concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor2:
         futures = []
         for loss in loss_function:
@@ -68,7 +68,7 @@ if __name__ == "__main__":
             
         # 等待所有任务完成
         concurrent.futures.wait(futures)
-    #"""
+    """
     with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor1, concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor2:
         
         futures = []
