7c876b9f9cf7f99d5fdab6d58f52fb11fde9de66
zhenlong-liu <reallong.liu@gmail.com>
2023-09-29 13:15:42 +0800

    230929_02_A100

--- mlh/examples/run_cmd_A100/search_0929_cifar10_resnet34_multi.py
+++ mlh/examples/run_cmd_A100/search_0929_cifar10_resnet34_multi.py
@@ -3,13 +3,11 @@ import concurrent.futures
 import sys
 sys.path.append("..")
 sys.path.append("../..")
-from generate_cmd import generate_cmd, generate_cmd_hup, generate_mia_command
-from mlh.examples.run_cmd_record.parameter_space_cifar100 import get_cifar100_parameter_set
-from run_cmd_record.parameter_space_cifar10 import get_cifar10_parameter_set
+from run_cmd.generate_cmd import generate_cmd, generate_cmd_hup, generate_mia_command
+from mlh.examples.run_cmd_record.parameter_space_cifar10 import get_cifar10_parameter_set
+from run_cmd_record.record import save_merged_dicts_to_yaml
 import GPUtil
 import time
-from run_cmd_record.record import save_merged_dicts_to_yaml
-
 def run_command(cmd):
     os.system(cmd)
 
@@ -29,35 +27,33 @@ if __name__ == "__main__":
     
     params = {
     'python': "../train_target_models_noinference.py",
-    "dataset": "CIFAR100",
-    "num_class": 100,
+    "dataset": "CIFAR10",
+    "num_class": 10,
     'log_path': '../save_adj', # '../save_p2'
     'training_type': 'NormalLoss',
     'loss_type': 'ce', # concave_log  concave_exp
-    'learning_rate': 0.1,
-    'epochs': 200, # 100
-    "model": "wide_resnet50",  # resnet18 # densenet121 # wide_resnet50
+    'learning_rate': 0.01,
+    'epochs': 120, # 100
+    "model": "resnet34",  # resnet18 # densenet121 # wide_resnet50 resnet34
     'optimizer' : "sgd",
     'seed' : 0,
     "alpha" : 1,
     "tau" : 1,
-    'scheduler' : 'multi_step_wide_resnet',
+    #'scheduler' : 'multi_step_wide_resnet',
     "temp" : 1,
     'batch_size' : 128,
     "num_workers" : 8,
     "loss_adjust" : None,
-    "gamma" :1.
+    "gamma" :1.0,
     }
     os.environ['MKL_THREADING_LAYER'] = 'GNU' 
     
-    #["concave_log","mixup_py"]
-    loss_function =["focal","ereg","ce_ls","flood"]
     
-
-
+    #loss_function =["concave_exp","concave_log","gce","flood","taylor","ce_ls","ereg","ereg","focal","ncemae"]
+    # "mixup_py","phuber"
+    loss_function =["sce"]
     
-
-    # Main loop
+    
     end_time = time.time() + 24*60*60  # 24 hours from now
     found_gpus = False
     while time.time() < end_time:
@@ -71,20 +67,17 @@ if __name__ == "__main__":
     if not found_gpus:
         print("Did not find suitable GPUs within 24 hours. Exiting the program.")
         exit()
-
     
-
-    
-    save_merged_dicts_to_yaml(params, loss_function, "./4090_record", dataset= params.get("dataset"))
+    save_merged_dicts_to_yaml(params, loss_function, "./A100_record")
     
     
-    gpu0 = gpu_ids[0]
-    gpu1 = gpu_ids[1]
+    
     with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor1, concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor2:
         futures = []
         for loss in loss_function:
             
-            param_dict = get_cifar100_parameter_set(loss)
+            param_dict = get_cifar10_parameter_set(loss)
+            print(param_dict)
             for temp in param_dict["temp"]:
                 for alpha in param_dict["alpha"]:
                     for gamma in param_dict["gamma"]:
@@ -96,20 +89,20 @@ if __name__ == "__main__":
                             params["gamma"] = gamma
                             params["tau"] = tau
                             
-                            cmd1, cmd2 = generate_cmd_hup(params,gpu0,gpu1)
+                            cmd1, cmd2 = generate_cmd_hup(params,0,1)
                             
                             
                             futures.append(executor1.submit(run_command, cmd1))
                             futures.append(executor2.submit(run_command, cmd2))
-             
+            
         # 等待所有任务完成
         concurrent.futures.wait(futures)
     
-    with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor1, concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor2:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor1, concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor2:
         
         futures = []
         for loss in loss_function:   
-            param_dict = get_cifar100_parameter_set(loss)
+            param_dict = get_cifar10_parameter_set(loss)
             for temp in param_dict["temp"]:
                 for alpha in param_dict["alpha"]:
                     for gamma in param_dict["gamma"]:
@@ -120,16 +113,15 @@ if __name__ == "__main__":
                             params["gamma"] = gamma
                             params["tau"] = tau
                             
-                            cmd3 =generate_mia_command(params, gpu = gpu0,  nohup = False, mia = "../mia_example_only_target.py")
-                            cmd4 = generate_mia_command(params, attack_type= "black-box", gpu = gpu1,  nohup = False, mia = "../mia_example_only_target.py")
+                            cmd3 =generate_mia_command(params, gpu = 0,  nohup = False, mia = "../mia_example_only_target.py")
+                            #cmd4 = generate_mia_command(params, attack_type= "black-box", gpu = 1,  nohup = False, mia = "../mia_example_only_target.py")
                             
                             futures.append(executor1.submit(run_command, cmd3))
-                            futures.append(executor2.submit(run_command, cmd4))
+                            #futures.append(executor2.submit(run_command, cmd4))
 
         concurrent.futures.wait(futures)
         # tmux kill-session -t 0
-        # tmux new -s 1
-        # conda activate mlh
-        # cd mlh/examples/run_cmd/
-        # python run_0928_cifar100_wideresnet_multi.py
-      
+        # tmux new -s <session-name>
+        # conda activate ml-hospital
+        # cd mlh/examples/run_cmd_A100/
+        # python search_0929_cifar10_resnet34_multi.py
