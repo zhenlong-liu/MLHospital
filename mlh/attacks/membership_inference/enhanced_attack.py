d312f92ef82b50dae42a2e747eb5044a0beb8648
lzl <stasustech@gmail.com>
2023-12-09 19:47:54 +0800

    aaa

---
+++ mlh/attacks/membership_inference/enhanced_attack.py
@@ -0,0 +1,11 @@
+from privacy_meter.audit import Audit
+from privacy_meter.dataset import Dataset
+from privacy_meter.hypothesis_test import threshold_func
+from privacy_meter.information_source import InformationSource
+from privacy_meter.information_source_signal import Signal
+from privacy_meter.model import Model
+from privacy_meter.metric import PopulationMetric
+from privacy_meter.constants import InferenceGame
+from privacy_meter.audit_report import ROCCurveReport, SignalHistogramReport
+
+
