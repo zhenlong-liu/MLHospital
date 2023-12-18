8ae66875c24cd83077ecc387d4f96f5a470f702c
liu <reallong.liu@gmail.com>
2023-12-18 04:56:21 -0800

    complete enhanced attack

--- mlh/utility/main_parse.py
+++ mlh/utility/main_parse.py
@@ -60,7 +60,10 @@ def add_argument_parameter(parser):
 
     parser.add_argument('--save_attack_path', type=str, default=None, help = 'path to save attack result')
 
-    parser.add_argument('--threshold_function', type=str, default='')
+    parser.add_argument('--fpr_tolerance_rate_list', metavar='F', type=float, nargs='*', default=[],
+                        help='an optional list of fpr tolerance rate')
+
+    parser.add_argument('--threshold_function', type=str, default='linear_itp_threshold_func',help='threshold_func, linear_itp_threshold_func, logit_rescale_threshold_func, gaussian_threshold_func, min_linear_logit_threshold_func')
 def save_namespace_to_yaml(namespace, output_path):
     """
     Save a Namespace object to a YAML file.
